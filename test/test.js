65ddf76597370737d6a44849687e6e31c3c26a5f
Julian Gruber <julian@juliangruber.com>
2013-08-01 17:18:45 -0400

    fix tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var data = require('..');
 var test = require('tape');
+var resolve = require('url').resolve;
 
 test('data-url-canvas', function (t) {
-  urlToDataUrl('/cat.png', function (url) {
+  urlToDataUrl(resolve(location.href, '/cat.png'), function (url) {
     data.toCanvas(url, function (canvas) {
       t.equal(url, data.fromCanvas(canvas));
       t.end();
